6cadeeb4c1830ac0bea012fb8c486bfea8f3d4c5
Philippe Marschall <philippe.marschall@gmail.com>
2019-08-26 17:41:21 +0200

    more methods

--- src/main/java/com/github/marschall/squeak/servlet/ServletNativeRequest.java
+++ src/main/java/com/github/marschall/squeak/servlet/ServletNativeRequest.java
@@ -2,8 +2,13 @@ package com.github.marschall.squeak.servlet;
 
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.Reader;
+import java.util.AbstractMap.SimpleImmutableEntry;
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.List;
+import java.util.Map.Entry;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -47,7 +52,7 @@ public final class ServletNativeRequest {
     }
     return requestURI + queryString;
   }
-  
+
   public String getRequestBodyAsString() throws IOException {
     StringBuilder builder = new StringBuilder();
     char[] buffer = new char[8192];
@@ -60,6 +65,34 @@ public final class ServletNativeRequest {
       }
     }
     return builder.toString();
+  }
+
+  public String getRequestAddressString() {
+    return this.request.getRemoteAddr();
+  }
+
+  public Cookie[] getCookies() {
+    return this.request.getCookies();
+  }
+  
+  public Entry<String, String[]>[] getRequestHeaders() {
+    List<Entry<String, String[]>> result = new ArrayList<>();
+    Enumeration<String> headerNames = this.request.getHeaderNames();
+    while (headerNames.hasMoreElements()) {
+      String headerName = headerNames.nextElement();
+      Enumeration<String> headers = request.getHeaders(headerName);
+      result.add(new SimpleImmutableEntry<>(headerName, toArray(headers)));
+    }
+    return (Entry<String, String[]>[]) result.toArray();
+  }
+  
+  private static String[] toArray(Enumeration<String> enumeration) {
+    List<String> result = new ArrayList<>(2);
+    while (enumeration.hasMoreElements()) {
+      String element =  enumeration.nextElement();
+      result.add(element);
+    }
+    return result.toArray(new String[0]);
   }
 
   public String getSslSessionId() {
